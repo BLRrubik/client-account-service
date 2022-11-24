74359edd1d6b45d8b5bd8364901e173c17a46cfa
roman <rubromvit@gmail.com>
2022-11-25 02:00:12 +0300

    update tasks

--- src/main/java/ru/rubik/client/client/Client.java
+++ src/main/java/ru/rubik/client/client/Client.java
@@ -1,6 +1,8 @@
 package ru.rubik.client.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import ru.rubik.client.request.AmountAddRequest;
@@ -8,7 +10,9 @@ import ru.rubik.client.request.AmountAddRequest;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -25,17 +29,17 @@ public class Client {
     public void run() throws InterruptedException {
 
         //количество читателей вызывающих метод getAmount(id)
-        Integer rCount = 10;
+        Integer rCount = 5;
         //количество читателей вызывающих метод addAmount(id,value)
-        Integer wCount = 10;
+        Integer wCount = 5;
 
-        List<Runnable> tasks = new ArrayList<>();
+        Set<Runnable> tasks = new HashSet<>();
 
         for (int i = 0; i < wCount; i++) {
             final int copyOfI = i;
             tasks.add(() -> {
                 try {
-                    restTemplate.postForObject(
+                    restTemplate.postForEntity(
                             new URI("http://localhost:8080/amount"),
                             new AmountAddRequest(1, 100L),
                             Long.class);
@@ -47,25 +51,26 @@ public class Client {
             });
         }
 
-//        for (int i = 0; i < rCount; i++) {
-//            final int copyOfI = i;
-//            tasks.add(() -> {
-//                try {
-//                    System.out.println( "get Ok " + copyOfI + " value " +
-//                            restTemplate.getForObject(new URI("http://localhost:8080/amount/1"), Long.class)
-//                    );
-//                } catch (URISyntaxException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            });
-//        }
+        for (int i = 0; i < rCount; i++) {
+            final int copyOfI = i;
+            tasks.add(() -> {
+                try {
+                    Long amount = restTemplate.getForObject(
+                            new URI("http://localhost:8080/amount/1"),
+                            Long.class);
+                    System.out.println("Get ok: " + copyOfI  + " value " + amount);
 
-
+                } catch (URISyntaxException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
 
         ExecutorService executor = Executors.newFixedThreadPool(wCount + rCount);
 
         for (Runnable task: tasks) {
             executor.execute(task);
+            Thread.sleep(400);
         }
 
         executor.shutdown();
